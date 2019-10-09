50d9d4fd29fce5a00c899d5ac0345a35a746ff70
John Cadena <cadena.john.85@gmail.com>
2019-10-09 15:51:42 -0500

    complete Tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date SMALLINT UNSIGNED,
+    sales DOUBLE,
+    genre VARCHAR(100),
+    PRIMARY KEY (id)
+);

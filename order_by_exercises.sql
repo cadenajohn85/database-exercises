805c6950e92cf227c4ad0dd9e60f4ddc516e1c96
John Cadena <cadena.john.85@gmail.com>
2019-10-11 11:59:40 -0500

    complete order_by_exercises.sql

---
+++ order_by_exercises.sql
@@ -0,0 +1,126 @@
+-- "order_by_exercises.sql"
+
+USE employees;
+
+-- 1.2.
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+-- 1.3
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+-- 1.4
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+-- 1.5
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+   OR last_name LIKE '%e'
+ORDER BY emp_no;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+  AND last_name LIKE '%e'
+ORDER BY emp_no;
+
+-- 1.6
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+   OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+  AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+-- 1.7
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+  AND birth_date LIKE '%-12-25'
+ORDER BY hire_date DESC,
+         birth_date DESC;
+
+##############################
+
+-- "where_exercises.sql"
+
+-- 1.1
+USE employees;
+
+-- 1.2
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+-- 1.3
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%';
+
+-- 1.4
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%';
+
+-- 1.5
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+-- 1.6
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+-- 2.1
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+-- 2.2
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+  AND gender = 'M';
+
+-- 2.3
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+   OR last_name LIKE '%e';
+
+-- 2.4
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+  AND last_name LIKE '%e';
+
+-- 2.5
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+  AND birth_date LIKE '%-12-25';
+
+-- 2.6
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
